a21842f685476def5757b99238100d8d265bf90a
Wiktor Kaczorowski <wkaczorowski@divante.pl>
2015-03-13 19:51:58 +0100

    Project skeleton

---
+++ app/code/community/Divante/LiveEditor/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wiktor Kaczorowski
+ * Date: 2015-03-13
+ * Time: 19:46
+ */ 
+class Divante_LiveEditor_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/Divante/LiveEditor/sql/divante_liveeditor_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wiktor Kaczorowski
+ * Date: 2015-03-13
+ * Time: 19:46
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
